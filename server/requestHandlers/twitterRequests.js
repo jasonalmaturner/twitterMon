200f1224d6555412ed61907d9978a3009a662acf
Jason <jasonalmaturner@gmail.com>
2015-04-24 11:54:59 -0600

    I'm going to not use most of this stuff I just wrote, but I wanted to push it anyways so the whole world can see my work.

--- server/requestHandlers/twitterRequests.js
+++ server/requestHandlers/twitterRequests.js
@@ -3,25 +3,65 @@ import q from 'q';
 import {constObj} from './../constants';
 
 var twitterRequests = {
-  getFollowers(screenName){
-    return new Promise(function(resolve, reject){
+  isFollower(userId, monName){
+
+  }
+};
+
+// Get stats of the potential tiwtterMon
+function getFollowing(userId, monName, nextCursor) {
+  return new Promise(function(resolve, reject){
+    new Twit({
+      consumer_key: constObj.twitterKey,
+      consumer_secret: constObj.twitterSecret,
+      access_token: req.session.passport.twitToken,
+      access_token_secret: req.session.passport.user.twitTokenSecret
+    }).get()
+  })
+}
+
+// Get list of ids that are following the potential twitterMon
+function twitFriends(monName, nextCursor, previousData){
+  return new Promise(function(resolve, reject){
+    if(nextCursor){
       new Twit({
         consumer_key: constObj.twitterKey,
         consumer_secret: constObj.twitterSecret,
         access_token: req.session.passport.twitToken,
         access_token_secret: req.session.passport.user.twitTokenSecret
-      }).get('followers/ids', {screen_name: screenName}, function(err, data, response){
+      }).get('friends/ids', {screen_name: monName, next_cursor: nextCursor}, function(err, data, respose){
         if(err){
-          reject(err);
+          var errObj = {
+            err: err,
+            collectedData: data
+          };
+          reject(errObj);
+        } else if(data.next_cursor){
+          twitFriends(userId, monName, nextCursor, data.next_cursor, data.ids.concat(previousData));
         } else {
-          console.log(response, data)
-          resolve();
+          resolve(data.ids.concat(previousData));
         }
       });
-    });
-  }
-};
+    } else {
+        new Twit({
+          consumer_key: constObj.twitterKey,
+          consumer_secret: constObj.twitterSecret,
+          access_token: req.session.passport.twitToken,
+          access_token_secret: req.session.passport.user.twitTokenSecret
+        }).get('friends/ids', {screen_name: monName}, function(err, data, response){
+          if(err){
+            reject(err);
+          } else if(data.next_cursor){
+            twitFriends(userId, monName, data.next_cursor, data.ids);
+          } else {
+            resolve(data.ids);
+          }
+        });
+    }
+  });
+}
 
-function isFollower (userId, monId) {
-
+// Check to see if the potential twitterMon is following the user
+function isFollower (userId, monFollowing) {
+    return monFollowing.indexOf(userId) !== 1;
 }
